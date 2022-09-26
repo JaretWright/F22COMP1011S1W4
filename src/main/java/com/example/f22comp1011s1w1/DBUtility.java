5d0c7121d95ad18feafb9d84ffa47d8e5d6d6fef
JaretWright <jaret.wright@georgiancollege.ca>
2022-09-26 13:24:10 -0400

    added a method in DBUtility to store a song

--- src/main/java/com/example/f22comp1011s1w1/DBUtility.java
+++ src/main/java/com/example/f22comp1011s1w1/DBUtility.java
@@ -1,9 +1,8 @@
 package com.example.f22comp1011s1w1;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import javafx.scene.Camera;
+
+import java.sql.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
@@ -52,4 +51,46 @@ public class DBUtility {
 
         return artists;
     }
+
+    /**
+     * This method will send a Song object into the DB and return the songID
+     */
+    public static int insertSongDB(Song song) throws SQLException {
+        int songID = -1;
+        ResultSet resultSet = null;
+
+        String sql = "INSERT INTO songs (name, genre, length, artistID) VALUES (?,?,?,?);";
+
+        //This is called a "try with resources" block.  It will autoclose anything in the ()
+        try(
+                Connection conn = DriverManager.getConnection(connectUrl,user,password);
+                PreparedStatement ps = conn.prepareStatement(sql, new String[] {"songID"})
+        )
+        {
+            //configure the prepared statement to prevent SQL injection attacks
+            ps.setString(1, song.getName());
+            ps.setString(2, song.getGenre());
+            ps.setInt(3, song.getLength());
+            ps.setInt(4, song.getArtist().getArtistID());
+
+            //run the command into the DB
+            ps.executeUpdate();
+
+            //get the cameraID
+            resultSet = ps.getGeneratedKeys();
+            while (resultSet.next())
+                songID = resultSet.getInt(1);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        finally {
+            if (resultSet != null)
+                resultSet.close();
+        }
+
+        return songID;
+    }
+
 }
