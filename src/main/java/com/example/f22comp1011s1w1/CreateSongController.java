6afd5d537d486c4f8cea30e22beb6be756c3914d
JaretWright <jaret.wright@georgiancollege.ca>
2022-10-03 11:09:07 -0400

    initial commit

--- src/main/java/com/example/f22comp1011s1w1/CreateSongController.java
+++ src/main/java/com/example/f22comp1011s1w1/CreateSongController.java
@@ -7,7 +7,9 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 
+import java.io.IOException;
 import java.net.URL;
+import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.ResourceBundle;
 
@@ -32,14 +34,17 @@ public class CreateSongController implements Initializable {
     private Spinner<Integer> yearSpinner;
 
     @FXML
-    void createSong(ActionEvent event) {
+    void createSong(ActionEvent event) throws SQLException {
         //when the submit button is pushed, create a Song object
         Song newSong = new Song(nameTextField.getText(),
                                 genreComboBox.getValue(),
                                 Integer.parseInt(lengthTextField.getText()),
                                 yearSpinner.getValue(),
                                 artistComboBox.getValue());
-        resultLabel.setText(newSong.toString());
+
+        //This line will add the new song object to the DB and return the songID
+        int songID = DBUtility.insertSongDB(newSong);
+        resultLabel.setText("Added to DB with songID =" + songID);
     }
 
     @Override
@@ -77,5 +82,10 @@ public class CreateSongController implements Initializable {
         });
 
         resultLabel.setText("");
+    }
+
+    @FXML
+    private void changeToTableView(ActionEvent event) throws IOException {
+        SceneChanger.changeScene(event, "song-table-view.fxml");
     }
 }
